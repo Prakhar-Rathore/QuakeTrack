23f1066ef25eb0d709b57458883ae5987c967326
Prakhar-Rathore <pralhar2000@gmail.com>
2021-06-21 18:38:50 +0530

    Removed redundant lines

--- app/src/main/java/com/example/quaketrack/QueryUtils.java
+++ app/src/main/java/com/example/quaketrack/QueryUtils.java
@@ -28,15 +28,11 @@ public final class QueryUtils {
     }
 
     public static List<Place> fetchEarthquakeData(String requestUrl) {
-        // Create URL object
         URL url = createUrl(requestUrl);
-
-        // Perform HTTP request to the URL and receive a JSON response back
         String jsonResponse = null;
         try {
             jsonResponse = makeHttpRequest(url);
         } catch (IOException e) {
-            //Log.e(LOG_TAG, "Problem making the HTTP request.", e);
         }
         return extractEarthquakes(jsonResponse);
     }
@@ -46,18 +42,13 @@ public final class QueryUtils {
         try {
             url = new URL(stringUrl);
         } catch (MalformedURLException e) {
-            //Log.e(LOG_TAG, "Problem building the URL ", e);
         }
         return url;
     }
 
-    /**
-     * Make an HTTP request to the given URL and return a String as the response.
-     */
     private static String makeHttpRequest(URL url) throws IOException {
         String jsonResponse = "";
 
-        // If the URL is null, then return early.
         if (url == null) {
             return jsonResponse;
         }
@@ -66,38 +57,28 @@ public final class QueryUtils {
         InputStream inputStream = null;
         try {
             urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
 
-            // If the request was successful (response code 200),
-            // then read the input stream and parse the response.
             if (urlConnection.getResponseCode() == 200) {
                 inputStream = urlConnection.getInputStream();
                 jsonResponse = readFromStream(inputStream);
-            }  //Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
 
         } catch (IOException e) {
-            //Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
             }
             if (inputStream != null) {
-                // Closing the input stream could throw an IOException, which is why
-                // the makeHttpRequest(URL url) method signature specifies than an IOException
-                // could be thrown.
                 inputStream.close();
             }
         }
         return jsonResponse;
     }
 
-    /**
-     * Convert the {@link InputStream} into a String which contains the
-     * whole JSON response from the server.
-     */
     private static String readFromStream(InputStream inputStream) throws IOException {
         StringBuilder output = new StringBuilder();
         if (inputStream != null) {
@@ -119,10 +100,8 @@ public final class QueryUtils {
             JSONArray features = root.getJSONArray("features");
             for(int i = 0; i < features.length(); i++) {
                 JSONObject properties = features.getJSONObject(i).getJSONObject("properties");
-                // getting time in the unix format
                 long time = Long.parseLong(properties.getString("time"));
                 Date date = new Date(time);
-                // getting concatenated location
                 String location = properties.getString("place");
                 String direction;
                 String place;
@@ -134,7 +113,6 @@ public final class QueryUtils {
                     direction = "0km N of";
                     place = location;
                 }
-                // getting magnitude in double format in making it 0.0 format
                 String magnitude = formatMagnitude(properties.getDouble("mag"));
                 earthquakes.add(new Place(magnitude, direction,
                         place, setDate(date), setTime(date), properties.getString("url")));
